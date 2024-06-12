588c34674a52799bf6a2ffd4d589f653ec0cbeac
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-06-12 17:17:50 +0200

    last commit pour immport VM

--- build_and_push_amd64.sh
+++ build_and_push_amd64.sh
@@ -1,37 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
-declare -A images
-images=(
-    ["frontend"]="./src/frontend"
-    ["cartservice"]="./src/cartservice/src"
-    ["productcatalogservice"]="./src/productcatalogservice"
-    ["currencyservice"]="./src/currencyservice"
-    ["paymentservice"]="./src/paymentservice"
-    ["shippingservice"]="./src/shippingservice"
-    ["emailservice"]="./src/emailservice"
-    ["checkoutservice"]="./src/checkoutservice"
-    ["recommendationservice"]="./src/recommendationservice"
-    ["adservice"]="./src/adservice"
-)
+# Définir les images et leurs chemins de contexte
+images="frontend:./src/frontend
+cartservice:./src/cartservice/src
+productcatalogservice:./src/productcatalogservice
+currencyservice:./src/currencyservice
+paymentservice:./src/paymentservice
+shippingservice:./src/shippingservice
+emailservice:./src/emailservice
+checkoutservice:./src/checkoutservice
+recommendationservice:./src/recommendationservice
+adservice:./src/adservice"
 
 # Construire et pousser toutes les images
-for image in "${!images[@]}"; do
-    context_path=${images[$image]}
-    image_name="registry.gitlab.unige.ch/filipe.ramos/k8s-project/${image}:amd64"
+for image_context in $images; do
+    image_name=$(echo $image_context | cut -d':' -f1)
+    context_path=$(echo $image_context | cut -d':' -f2)
+    full_image_name="registry.gitlab.unige.ch/filipe.ramos/k8s-project/${image_name}:amd64"
     
-    echo "Building ${image_name} from ${context_path}..."
-    docker buildx build -t ${image_name} ${context_path} --platform=linux/amd64 --load
+    echo "Building ${full_image_name} from ${context_path}..."
+    docker build -t ${full_image_name} ${context_path} --platform=linux/amd64
     if [ $? -ne 0 ]; then
-        echo "Failed to build ${image_name}"
+        echo "Failed to build ${full_image_name}"
         exit 1
     fi
-    echo "Successfully built ${image_name}"
+    echo "Successfully built ${full_image_name}"
     
-    echo "Pushing ${image_name}..."
-    docker push ${image_name}
+    echo "Pushing ${full_image_name}..."
+    docker push ${full_image_name}
     if [ $? -ne 0 ]; then
-        echo "Failed to push ${image_name}"
+        echo "Failed to push ${full_image_name}"
         exit 1
     fi
-    echo "Successfully pushed ${image_name}"
+    echo "Successfully pushed ${full_image_name}"
 done
