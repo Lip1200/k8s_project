210e7bb60fe61b47670192ae74cd247f467c4bd5
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-06-14 15:46:31 +0200

    remise à jour des images

--- build_and_push_amd64.sh
+++ build_and_push_amd64.sh
@@ -16,10 +16,10 @@ adservice:./src/adservice"
 for image_context in $images; do
     image_name=$(echo $image_context | cut -d':' -f1)
     context_path=$(echo $image_context | cut -d':' -f2)
-    full_image_name="registry.gitlab.unige.ch/filipe.ramos/k8s-project/${image_name}:amd64"
+    full_image_name="registry.gitlab.unige.ch/filipe.ramos/k8s-project/${image_name}:latest"
     
     echo "Building ${full_image_name} from ${context_path}..."
-    docker build -t ${full_image_name} ${context_path} --platform=linux/amd64
+    docker build -t ${full_image_name} ${context_path}
     if [ $? -ne 0 ]; then
         echo "Failed to build ${full_image_name}"
         exit 1
